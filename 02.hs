aa99242f94c66afed256b9c3154dddec40b232d0
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-13 12:55:52 -0500

    13/09

---
+++ 02.hs
@@ -0,0 +1,17 @@
+{-****************************
+  * 02.hs             *
+  ****************************-}
+--Funcion de suma 
+suma x y = x + y
+--Funcion de resta 
+resta x y = x - y
+--Funcion de media aritmetica de tres numeros 
+mediaTres x y z = (x + y + z) / 3
+--Funcion potencia de un numero 
+potencia x = x * x 
+--Funcion de maximo de tres numeros
+maxTres x y z = max x (max y z)
+--Funcion de minimo de tres numeros
+minTres x y z = min x (min y z)
+--Funcion verifica si tres numeros son iguales 
+--igualTres x y z = x == y && = z
