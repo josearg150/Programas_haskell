e425d39b7544bad9f4faf07b4f5511e4ec5a2dec
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-15 12:55:58 -0500

    15/09

---
+++ 01_mifunc.hs
@@ -0,0 +1,8 @@
+{-****************************
+  * 04.hs                    
+  ****************************-}
+--Mi funcion drop
+--Aprender if y sangria en haskell
+mifunc n xs = if n <= 0 || null xs
+              then xs
+              else mifunc (n-1) (tail xs)
