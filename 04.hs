bd55cff58284b34f98dbdafdeb61ab3cf4a236a8
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-27 12:34:25 -0500

    a

---
+++ 04.hs
@@ -0,0 +1,10 @@
+{-****************************
+  * 04.hs Funciones
+  Autor: jose angel rocha garcia
+  ****************************-}
+--if
+    fun x = if x > 10
+               then x
+               else x * 2
+--if
+    fun' x = if x > 10 then x else x * 2 + 1
